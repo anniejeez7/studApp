1d436abb89deacc165bb59a587f3df87e852b3e0
Jeez <658633@cognizant.com>
2018-06-25 18:02:02 +0530

    Student application files added

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
         .state('home', {
-            url: '/home',
+            url: '/getStudents',
             templateUrl: '/static/view-student.html'
         })
         .state('addStudent', {
@@ -15,106 +15,126 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl: '/static/edit-student.html'
         });
 });
-app.factory("StudentService", function ($filter,$http) {
+app.factory("StudentService", function ($q, $filter, $http) {
     return new function () {
         var studentInfo = {};
         studentInfo.studentList = [];
-        studentInfo.addStudent = function (indexId, id, name, dob) {
-            studentInfo.studentList.push({ indexId, id, name, dob });
-			/*MongoClient.connect(url, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db("mydb");
-			  var myobj = { indexId:indexId,id:id,name: name,dob:dob};
-			  dbo.collection("studoo").insertOne(myobj, function(err, res) {
-				if (err) throw err;
-				console.log("1 document inserted");
-				db.close();
-			  });
-			});*/
-			$http({
-			  method: 'POST',
-			  url: '/addStudent',
-			  headers: {
-			   'Content-Type': "application/json"
-			 },
-			  data: JSON.stringify({indexId:indexId,id:id,name: name,dob:dob})
-			}).success(function(response) {
-			  console.log('student added');
-			  $scope.response = response;
-			});
+        studentInfo.addStudent = function (id, name, dob) {
+            studentInfo.studentList.push({ id, name, dob });
+            $http({
+                method: 'POST',
+                url: '/addStudent',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: JSON.stringify({ id: id, name: name, dob: dob })
+            }).success(function (response) {
+                console.log('student added');
+            });
         }
         studentInfo.viewStudents = function () {
-			$http({
-			  method: 'GET',
-			  url: '/getStudents',
-			  headers: {
-			   'Content-Type': "application/json"
-			 },
-			}).success(function(response) {
-			  console.log('students received');
-			  console.log(response[0]);
-			  var temp=[];
-			  for(var i=0; i < response.length; i++){
-				  temp.push({indexId:response[i].indexId,id:response[i].id,name: response[i].name,dob:response[i].dob});
-			  }
-			  studentInfo.studentList=temp;
-			  console.log(studentInfo.studentList);
-		}).error(function(res,err){ console.log("error")});
-            return studentInfo.studentList;
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/getStudents',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+            }).success(function (response) {
+                console.log('students received');
+                var temp = [];
+                for (var i = 0; i < response.length; i++) {
+                    temp.push({ _id: response[i]._id, id: response[i].id, name: response[i].name, dob: response[i].dob });
+                }
+                studentInfo.studentList = temp;
+                console.log("1 studentList bf")
+                console.log(studentInfo.studentList);
+                console.log("1 studentList af")
+                deferred.resolve(studentInfo.studentList);
+            }).error(function (res, err) { console.log("error"); deferred.reject("Rejected"); });
+            return deferred.promise;
         }
-        studentInfo.getStudent = function (indexId) {
+        studentInfo.getStudent = function (_id) {
             console.log("In getStudent");
-            console.log("Index id " + indexId);
             console.log(studentInfo.studentList);
-            var position=studentInfo.studentList.map(function(e){ return e.indexId; }).indexOf(indexId);
-            console.log("POSITION "+position);
+            var position = studentInfo.studentList.map(function (e) { return e._id; }).indexOf(_id);
+            console.log("POSITION " + position);
             return studentInfo.studentList[position];
         }
         studentInfo.editStudent = function (student) {
-            var position=studentInfo.studentList.map(function(e){ return e.indexId; }).indexOf(student.indexId);
+            var position = studentInfo.studentList.map(function (e) { return e._id; }).indexOf(student._id);
             console.log(position);
             studentInfo.studentList[position] = student;
+            $http({
+                method: 'POST',
+                url: '/updateStudent',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: JSON.stringify({ _id: student._id, id: student.id, name: student.name, dob: student.dob })
+            }).success(function (response) {
+                console.log("Successfully updated");
+            });
         }
-        studentInfo.deleteStudent=function(indexId){
+        studentInfo.deleteStudent = function (_id) {
             // console.log(studentInfo.studentList[indexId]);
-            var position=studentInfo.studentList.map(function(e){ return e.indexId; }).indexOf(indexId);
-            studentInfo.studentList.splice(position,1);
+            var position = studentInfo.studentList.map(function (e) { return e._id; }).indexOf(_id);
+            console.log(position);
+            studentInfo.studentList.splice(position, 1);
+            $http({
+                method: 'POST',
+                url: '/deleteStudent',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: JSON.stringify({ _id: _id})
+            }).success(function (response) {
+                console.log("Successfully deleted");
+            });
         }
         return studentInfo;
     }
 });
 app.controller("MainController", function ($scope, $location, $route, $window, StudentService) {
-    var indexId = 0;
     console.log("In MainController");
-    $scope.incrementIndexId = function () {
-        indexId++;
+    $scope.onloadFun=function(){
+        console.log("In init");
+        $scope.students = StudentService.viewStudents();
     }
     $scope.addStudent = function (id, name, dob) {
-        console.log("Add student " + indexId + " " + id + " " + name + " " + dob);
-        StudentService.addStudent(indexId, id, name, dob);
-        $scope.incrementIndexId();
+        console.log("Add student " + " " + id + " " + name + " " + dob);
+        StudentService.addStudent(id, name, dob);
         $scope.students = StudentService.viewStudents();
-        console.log($location.url());
+        // console.log("Scope students after StudentService call");
+        // console.log($scope.students);
         $location.url('/home');
-        console.log($location.url());
     }
     $scope.viewStudents = function () {
         $scope.students = StudentService.viewStudents();
+        console.log("In controller view students");
+        console.log($scope.students);
     }
-    $scope.editStudent = function (indexId) {
+    $scope.editStudent = function (_id) {
         console.log("In edit Student");
-        $scope.student = StudentService.getStudent(indexId);
+        $scope.student = [];
+        $scope.student = StudentService.getStudent(_id);
         console.log($scope.student);
-        $scope.submit = StudentService.editStudent($scope.student);
+        // $scope.submit = StudentService.editStudent($scope.student);
+        $scope.submit = function () {
+            console.log($scope.student);
+            StudentService.editStudent($scope.student);
+        }
+        console.log($scope.student);
         $scope.students = StudentService.viewStudents();
     }
 
-    $scope.deleteStudent=function(indexId){
-        console.log("deleteStudent "+indexId);
-        StudentService.deleteStudent(indexId);
+    $scope.deleteStudent = function (_id) {
+        console.log("deleteStudent " + _id);
+        StudentService.deleteStudent(_id);
         $scope.students = StudentService.viewStudents();
     }
 
+    // viewStudents();
 });
 app.directive("myStudent", function () {
     return {
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, db) {
+// var url = "mongodb://localhost:27017/";
+var url = "mongodb://ann:userann123@ds161520.mlab.com:61520/mongo_angular"
+var ObjectID = require('mongodb').ObjectID;
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
- var dbo = db.db("mydb");
-  dbo.createCollection("students", function(err, res) {
+  var dbo = db.db("mongo_angular");
+  dbo.createCollection("students", function (err, res) {
     if (err) throw err;
     console.log("Collection created!");
     db.close();
@@ -16,41 +17,76 @@ const express = require('express');
 var bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.listen(3000, function() {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, function () {
   console.log('listening on 3000');
 });
-app.use('/static', express.static(__dirname+ '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 //app.use('/ui-router.js',require(__dirname+'/ui-router.js'));
-app.get('/', function(req, res) {
-res.sendFile(__dirname + '/index.html')
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
 });
-app.post('/addStudent',function(req, res){	
-	console.log("In POST");
-	console.log(req.body);
-	MongoClient.connect(url, function(err, db) {
-	  if (err) throw err;
-	 var dbo = db.db("mydb");
-	dbo.collection("students").insertOne(req.body, function(err, res) {
+app.post('/addStudent', function (req, res) {
+  console.log("In POST");
+  console.log(req.body);
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    console.log("1 student inserted");
-    db.close();
-  });
-	});
-});
-
-app.get('/getStudents',function(req, res){
-	console.log("In GET");
-	var resultSet;
-	MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.collection("students").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    //console.log(result);
-    db.close();
-		res.send(result);
+    var dbo = db.db("mongo_angular");
+    dbo.collection("students").insertOne(req.body, function (err, res) {
+      if (err) throw err;
+      console.log("1 student inserted");
+      db.close();
+    });
   });
 });
 
-});
+app.get('/getStudents', function (req, res) {
+  console.log("In GET");
+  var resultSet;
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("mongo_angular");
+    dbo.collection("students").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      //console.log(result);
+      db.close();
+      res.send(result);
+    });
+  });
+  app.post('/updateStudent', function (req, res) {
+    console.log("In UPDATE");
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("mongo_angular");
+      console.log("Req ID ");
+      console.log(req.body);
+      var myquery = { _id: ObjectID(req.body._id) };
+      var newvalues = { $set: { name: req.body.name, id: req.body.id, dob: req.body.dob } };
+      var query = { _id: ObjectID(req.body._id) };
+      dbo.collection("students").find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
+      dbo.collection("students").updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        db.close();
+      });
+    });
+  });
+    app.post('/deleteStudent', function (req, res) {
+      console.log("In DELETE");
+      MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("mongo_angular");
+        console.log("Req ID ");
+        var myquery = { _id: ObjectID(req.body._id) };
+        dbo.collection("students").deleteOne(myquery, function (err, obj) {
+          if (err) throw err;
+          console.log("1 document deleted");
+          db.close();
+        });
+      });
+    });
+  });
